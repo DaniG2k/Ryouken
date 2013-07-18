5ddbf5d69e8e3e7e41308316061d2aa0ad0ecd99
DaniG2k <byakugan.87@gmail.com>
2013-07-18 16:22:21 +0100

    Added rm_secondary_urls! method

--- ryouken.rb
+++ ryouken.rb
@@ -7,12 +7,13 @@ class Ryouken
   
   def initialize(url)
     @url = url
-    @uri = URI(url)
-    @base_url = @uri.scheme + '://' + @uri.host
+    uri = URI(url)
+    @host = uri.scheme + '://' + uri.host
   end
   
   def crawl
     children = process_page(@url)
+    rm_secondary_urls!(children, @host)
   end
   
   def process_page(url)
@@ -20,6 +21,7 @@ class Ryouken
     get_links(@url, page)
   end
   
+  # Return an array of links containing scheme+host+path
   def get_links(url, page)
     links = []
     page.css('a').map do |link|
@@ -42,10 +44,16 @@ class Ryouken
     end
   end
   
-  def make_absolute(url, new_url)
-    return @base_url + new_url
+  def make_absolute(url, path)
+    @host + path
+  end
+  
+  def rm_secondary_urls!(url_list, host)
+    primary_urls = []
+    url_list.each {|url| primary_urls.push(url) if url.start_with?(host) }
+    primary_urls
   end
 end
 
-hound = Ryouken.new('http://www.asia-gazette.com')
+hound = Ryouken.new('http://www.bbc.co.uk')
 puts hound.crawl
