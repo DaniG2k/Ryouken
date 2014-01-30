700636dea3f8da2b0187501989c37505159626bf
Daniele <byakugan.87@gmail.com>
2014-01-30 12:18:33 +0000

    Update ryouken2.rb

--- ryouken2.rb
+++ ryouken2.rb
@@ -4,25 +4,40 @@ require 'nokogiri'
 
 class Ryouken
   def initialize(seed)
-    uri = URI(seed)
-    @seed = seed
-    @base_url = "#{uri.scheme}://#{uri.host}" 
+    @seed = URL.new(seed)
   end
   
   def get_links
     doc = Nokogiri::HTML(open(@seed))
+    #doc.xpath("//a/@href").map(&:text).uniq
     urls = doc.css('a').map { |href| href.attribute('href').text }.uniq
-    urls.each do |url|
-      puts url.make_absolute
-    end
+    urls.each { |url| puts url.absolute(base: @seed) }
+  end
+end
+
+class URL
+  def initialize(url)
+    @url = url
   end
   
-  def make_absolute
-    self.is_relative? ? "#{@base_url}#{self}" : self
+  def to_s
+    @url.to_s
+  end
+  
+  def +(rhs)
+    URL.new(to_s + rhs.to_s)
+  end
+  
+  def ==(rhs)
+    @url == rhs.to_s
+  end
+  
+  def absolute(base: nil)
+    is_relative? ? base + self : self
   end
   
   def is_relative?
-    (self =~ /^\//).nil? ? false : true 
+    @url =~ /^\// ? true : false 
   end
 end
 
