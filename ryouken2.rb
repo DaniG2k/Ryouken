a119d67203dd3333864b9a2885e76786f32f6233
Daniele <byakugan.87@gmail.com>
2014-01-31 16:38:31 +0000

    Update ryouken2.rb

--- ryouken2.rb
+++ ryouken2.rb
@@ -1,18 +1,17 @@
 require 'open-uri'
 require 'nokogiri'
-require 'set'
 
-class Crawler
+class Ryouken
   class PageProcessor
     attr_reader :url, :dom
     def initialize(url)
-      @url = url
+      @url = url.to_s # url is a URI object
       @dom = open(@url) { |fh| Nokogiri::HTML(fh) }
     end
     
     # Get all hrefs and return in @dom and return them as a set.
     def urls
-      @dom.xpath("//a/@href").map { |node| rewrite_url(node.text) }.to_set
+      @dom.xpath("//a/@href").map { |node| rewrite_url(node.text) }.uniq
     end
     
     # In the event a url is relative (ex. '/') rewrite it to a full path
@@ -25,13 +24,14 @@ class Crawler
     end
   end
   
-  attr_reader :start_url, :include_patterns, :exclude_patterns, :visited, :work
-  def initialize(start_url, include: nil, exclude: nil)
-    @start_uri        = URI(start_url)
-    @include_patterns = include && /#{Regexp.quote(include)}/
-    @exclude_patterns = exclude && /#{Regexp.quote(exclude)}/
-    @visited          = Set.new
-    @work             = [@start_url]
+  attr_reader :start_url, :include_ptn, :exclude_ptn, :visited, :work
+  def initialize(start_url, include_ptn: nil, exclude_ptn: nil)
+    @start_url    = URI(start_url)
+    @visited      = Array.new
+    @work         = [@start_url]
+    # The && is for returning nil if no value was specified
+    @include_ptn  = include_ptn && /#{Regexp.quote(include_ptn)}/
+    @exclude_ptn  = exclude_ptn && /#{Regexp.quote(exclude_ptn)}/
   end
   
   def run
@@ -45,15 +45,9 @@ class Crawler
   end
   
   def append_urls(urls)
-    remaining = urls - @visited - @work.to_set
-    remaining.select! { |url| url.to_s =~ @include_patterns } if @include_patterns
-    remaining.reject! { |url| url.to_s =~ @exclude_patterns } if @exclude_patterns
-    remaining.select! { |url| ['http', 'https'].include?(url.scheme) }
-    @work.concat(remaining.to_a)
+    remaining = urls - @visited - @work
+    remaining.select! { |url| url =~ @include_ptn } if @include_ptn
+    remaining.reject! { |url| url =~ @exclude_ptn } if @exclude_ptn
+    @work << remaining
   end
 end
-
-
-#my_crawler = Crawler.new('https://www.asia-gazette.com', include_patterns: 'asia-gazette')
-my_crawler = Crawler.new 'http://www.nytimes.com/pages/world/asia/', include: 'ref=asia'
-my_crawler.run { |page| puts "Visiting #{page.url}" }
