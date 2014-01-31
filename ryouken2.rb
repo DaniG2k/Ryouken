c3939b6ad5b6d992090749ea76268698be81d260
Daniele <byakugan.87@gmail.com>
2014-01-31 16:02:59 +0000

    Update ryouken2.rb

--- ryouken2.rb
+++ ryouken2.rb
@@ -1,33 +1,59 @@
-#!/usr/bin/ruby
 require 'open-uri'
 require 'nokogiri'
+require 'set'
 
-class Ryouken
-  def initialize(seed, include_patterns: nil, exclude_patterns: nil)
-    uri = URI seed
-    @seed = seed
-    @base = "#{uri.scheme}://#{uri.host}"
-    @include_patterns = include_patterns
-    @exclude_patterns = exclude_patterns
+class Crawler
+  class PageProcessor
+    attr_reader :url, :dom
+    def initialize(url)
+      @url = url
+      @dom = open(@url) { |fh| Nokogiri::HTML(fh) }
+    end
+    
+    # Get all hrefs and return in @dom and return them as a set.
+    def urls
+      @dom.xpath("//a/@href").map { |node| rewrite_url(node.text) }.to_set
+    end
+    
+    # In the event a url is relative (ex. '/') rewrite it to a full path
+    def rewrite_url(path)
+      is_relative?(url) ? @url + path : path
+    end
+    
+    def is_relative?(url)
+      url =~ /^\// ? true : false
+    end
   end
   
-  def get_urls
-    doc = Nokogiri::HTML(open(@seed))
-    doc.xpath("//a/@href").map(&:text).uniq
+  attr_reader :start_url, :include_patterns, :exclude_patterns, :visited, :work
+  def initialize(start_url, include: nil, exclude: nil)
+    @start_uri        = URI(start_url)
+    @include_patterns = include && /#{Regexp.quote(include)}/
+    @exclude_patterns = exclude && /#{Regexp.quote(exclude)}/
+    @visited          = Set.new
+    @work             = [@start_url]
   end
   
-  def include_urls(url_array)
-    url_array.select {|url| url =~ /#{Regexp.quote(@include_patterns)}/} if @include_patterns
+  def run
+    until @work.empty?
+      current_url = @work.shift
+      @visited << current_url
+      page = PageProcessor.new(current_url)
+      yield(page)
+      append_urls(page.urls)
+    end
   end
   
-  def exclude_urls(url_array)
-    url_array.reject {|url| url =~ /#{Regexp.quote(@exclude_patterns)}/} if @exclude_patterns
-  end
-  
-  def is_relative?(url)
-    url =~ /^\// ? true : false
+  def append_urls(urls)
+    remaining = urls - @visited - @work.to_set
+    remaining.select! { |url| url.to_s =~ @include_patterns } if @include_patterns
+    remaining.reject! { |url| url.to_s =~ @exclude_patterns } if @exclude_patterns
+    remaining.select! { |url| ['http', 'https'].include?(url.scheme) }
+    @work.concat(remaining.to_a)
   end
 end
 
-seed = Ryouken.new("http://www.asia-gazette.com/", include_patterns: 'www.asia-gazette.com')
-seed.get_urls
+
+#my_crawler = Crawler.new('https://www.asia-gazette.com', include_patterns: 'asia-gazette')
+my_crawler = Crawler.new 'http://www.nytimes.com/pages/world/asia/', include: 'ref=asia'
+my_crawler.run { |page| puts "Visiting #{page.url}" }
